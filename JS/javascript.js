d19cd077bb43e56d2f15ee58f960ae061d1814c8
Lisien <lisienskenderi@hotmail.com>
2023-01-18 22:29:51 +0100

    Menu Utente per mobile e ottimizzato il javascript

--- JS/javascript.js
+++ JS/javascript.js
@@ -7,29 +7,39 @@ function userMenu() {
 }
 
 function PageLoad() {
+  // sessionStorage.setItem('cambiato', 'false');
   if(document.getElementById("areaPersonale") != null){
     sessionStorage.setItem('loggedIn', 'true');
-    // sessionStorage.setItem('cambiato', 'false');
+    sessionStorage.setItem('cambiato', 'false');
     console.log('H1');
   }
+  if('cambiato' in sessionStorage){
+    sessionStorage.setItem('cambiato', 'false');
+  }
 
-  if(sessionStorage.getItem('loggedIn') == 'true'){
-    document.getElementById("LogIn").style.display = "none";
-    document.getElementById("LogOut").style.display = "block";
-    document.getElementById("LogOut").style.border = "none";
-    document.getElementById("LogOut").style.padding = "0";
-    // sessionStorage.setItem('cambiato', 'true');
-  } else {
-    document.getElementById("LogIn").style.display = "block";
-    document.getElementById("LogIn").style.border = "none";
-    document.getElementById("LogIn").style.padding = "0";
-    document.getElementById("LogOut").style.display = "none";
+  if(sessionStorage.getItem('cambiato') == 'false' && sessionStorage.getItem('loggedIn') == 'true'){
+    // document.getElementById("LogIn").style.display = "none";
+    // document.getElementById("LogOut").style.display = "block";
+    // document.getElementById("LogOut").style.border = "none";
+    // document.getElementById("LogOut").style.padding = "0";
+    document.getElementById("LogIn").classList.add('hide');
+    document.getElementById("LogOut").classList.add('menuLogOutUt');
+    sessionStorage.setItem('cambiato', 'true');
+  } else if (sessionStorage.getItem('cambiato') == 'false'){
+    // document.getElementById("LogIn").style.display = "block";
+    // document.getElementById("LogIn").style.border = "none";
+    // document.getElementById("LogIn").style.padding = "0";
+    // document.getElementById("LogOut").style.display = "none";
+    document.getElementById("LogIn").classList.add("menuLogInUt");
+    document.getElementById("LogOut").classList.add('hide');
+    sessionStorage.setItem('cambiato', 'true');
   }
   
 }
 
 function LogOut() {
   sessionStorage.setItem('loggedIn', 'false');
+  sessionStorage.setItem('cambiato', 'false');
 }
   
 
@@ -56,5 +66,44 @@ document.addEventListener("click", function(event){
   } 
 });
 
+document.addEventListener("scroll", function(event){
+  let scrollBrn = document.querySelector("#top-right");
+  let footer = document.querySelector("footer");
+  let body = document.querySelector("body");
+  const  x = window.matchMedia("(max-width: 750px)").matches;
+  const y = (body.offsetHeight - footer.offsetHeight - 90 );
+  if(x){
+    if((window.scrollY + window.innerHeight) >= (document.documentElement.scrollHeight - footer.offsetHeight)){
+      scrollBrn.classList.remove("menuLogMob");
+      scrollBrn.classList.add("menuLogMobStop");
+      // scrollBrn.style.position = "absolute";
+      // let classStyle = document.getElementsByClassName("menuLogMobStop");
+      let topRule = new Array();
+        if (document.styleSheets[4].cssRules[6].cssRules[3]) {
+            topRule = document.styleSheets[4].cssRules[6].cssRules[3];
+        } else if (document.styleSheets[4].rules[6].rules[3]) { // StackOverflow diceva che alcuni browser usavano .rules NON TOGLIERE!
+            topRule = document.styleSheets[4].rules[6].rules[3];
+        }
+        topRule.style.top = y + 'px';
+        // theRules.style.righ
+      // scrollBrn.style.top = (y) + "px";
+      // scrollBrn.style.bottom = -60 + "em";
+    } else {
+      scrollBrn.classList.remove("menuLogMobStop");
+      scrollBrn.classList.add("menuLogMob")
+      // scrollBrn.style.position = "fixed";
+      // scrollBrn.style.bottom = 2 + "em";
+      // scrollBrn.style.top = "initial";
+      // console.log('HIII');
+      // console.log(document.body.clientHeight);
+      // console.log(footer.offsetHeight);
+      // console.log(window.scrollY);
+      }
+  } else {
+    scrollBrn.classList.remove("menuLogMobStop");
+    scrollBrn.classList.remove("menuLogMob");
+    scrollBrn.classList.add("menuLogDesk");
+  }
+});
 
 
